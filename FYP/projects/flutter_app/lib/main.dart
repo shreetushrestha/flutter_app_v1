4938858dbd4f1d59e49c18b9bba19fed09efd108
Shreetu Shrestha <shreetu@Shreetus-MacBook-Air.local>
2025-08-06 16:24:54 +0545

    Updated Row layout and added image

--- FYP/projects/flutter_app/lib/main.dart
+++ FYP/projects/flutter_app/lib/main.dart
@@ -4,6 +4,19 @@ void main() {
   runApp(const MyApp());
 }
 
+int a = 1;
+String b = "wolrd";
+double hh = 2.0;
+List l = [1, 2, 3, 'ow'];
+Map m = {
+  'string': 'hello',
+  'int': 1,
+  'double': 2.0,
+  'list': [1, 2, 3],
+};
+// This is a basic Flutter application that demonstrates the use of widgets,
+// state management, and theming.
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -28,7 +41,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -85,38 +98,43 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: Row(
+        children: [
+          Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+              margin: const EdgeInsets.all(5),
+              color: Colors.blue,
+              width: 200,
+              height: 100,
+              alignment: Alignment.center, // centers the text without Center()
+              child: Row(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center, // centers the text vertically
+                    children: [
+                      Text('halo', style:TextStyle(fontSize: 10)),
+                      Text('2',style: TextStyle(fontSize: 20)),
+                    ]
+                  ),
+                ],
+              )
+                  ),
+                ],
+                ),
+            ],
+          ),
+          Image.network('https://i.pinimg.com/736x/98/44/72/9844726e937432e74c85db3ecb753da2.jpg',
+            width: 200,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
